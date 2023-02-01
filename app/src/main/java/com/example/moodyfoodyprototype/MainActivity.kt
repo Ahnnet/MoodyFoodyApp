aeb8be3f3464eecc4e4b26acf9b4c0a981fd042d
Ahnnet <drj9802@gmail.com>
2023-02-01 17:07:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/moodyfoodyprototype/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.moodyfoodyprototype
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.moodyfoodyprototype.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* user image upload */
+        binding.btnUpload.setOnClickListener {
+//            ActivityCompat.requirePermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE), 101)
+            permission()
+            loadImage()
+
+        }
+
+        /* button analyze */
+        binding.btnAnalyze.setOnClickListener {
+            var intent = Intent(this, MoodyActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /* Image load function */
+    private fun loadImage(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Load Picture"), 0)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0){
+            if(resultCode == RESULT_OK){
+                var dataUri = data?.data
+                try{
+                    /* get the image to 'userPhoto' */
+                    var userPhoto : Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, dataUri)
+                    binding.imgUser.setImageBitmap(userPhoto)
+                }catch(e:Exception){
+                    Toast.makeText(this,"$e",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"album access failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    /* request album access permission */
+    private fun permission(){
+        var readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        /* when the permission was denied -> request permission*/
+        if(readPermission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                0)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moodyfoodyprototype/MoodyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moodyfoodyprototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moody)
+    }
+}
